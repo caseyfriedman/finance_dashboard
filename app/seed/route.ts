592b808b96dbdddf62238f30a19cb06613cbda08
caseyfriedman <56453433+caseyfriedman@users.noreply.github.com>
2025-05-19 09:18:30 -0600

    Updates

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -4,6 +4,12 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
+
+
+
+console.log("Connected to postgres?");
+
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
@@ -15,6 +21,7 @@ async function seedUsers() {
     );
   `;
 
+  console.log("inserting users....");
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
@@ -25,6 +32,7 @@ async function seedUsers() {
       `;
     }),
   );
+  console.log("done inserting users....");
 
   return insertedUsers;
 }
@@ -105,11 +113,10 @@ export async function GET() {
   try {
     const result = await sql.begin((sql) => [
       seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+      ]);
 
+
+    console.log("finished sql");
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
